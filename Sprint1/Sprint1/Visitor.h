858073b9d3c8522c09bf21c09e72e202ca810814
ThomasMalvoisin <37004976+ThomasMalvoisin@users.noreply.github.com>
2019-03-22 09:45:54 +0100

    Cleaned repo and completed .gitignore (#2)
    * [REWORK] gitignore
    
    * [REWORK] gitignore 2 & bug fix
    
    * deleted .antlr

--- Sprint1/Sprint1/Visitor.h
+++ Sprint1/Sprint1/Visitor.h
@@ -1,21 +1,21 @@
 #pragma once
 #include "antlr4-runtime.h"
-#include "Sprint1BaseVisitor.h"
+#include "Grammar1BaseVisitor.h"
 using namespace std;
-class Visitor : public Sprint1BaseVisitor
+class Visitor : public Grammar1BaseVisitor
 {
     public:
-      virtual antlrcpp::Any visitProg(Sprint1Parser::ProgContext *ctx) override
+      virtual antlrcpp::Any visitProg(Grammar1Parser::ProgContext *ctx) override
       {
             return (int)visit(ctx->main());
       }
 
-      virtual antlrcpp::Any visitMain(Sprint1Parser::MainContext *ctx) override
+      virtual antlrcpp::Any visitMain(Grammar1Parser::MainContext *ctx) override
       {
             return (int)visit(ctx->body());
       }
 
-      virtual antlrcpp::Any visitBody(Sprint1Parser::BodyContext *ctx) override
+      virtual antlrcpp::Any visitBody(Grammar1Parser::BodyContext *ctx) override
       {
             return (int)atoi(ctx->INT()->getText().c_str());
       }
