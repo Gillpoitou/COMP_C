aa2b2d3f19e7bceca9804d6bab577b7539616673
Gillpoitou <etibossk@gmail.com>
2019-03-27 10:12:02 +0100

    [S2-ASS][add]Function buildASM

--- Sprint2/Sprint2/Function.cpp
+++ Sprint2/Sprint2/Function.cpp
@@ -1,5 +1,19 @@
 #include "Function.h"
 
 void Function::buildASM(ostream &o){
-    
+
+    int N;
+    N = declarations->size()*16;
+      o << "enter " << N  << ", 0" << endl;
+
+      for(Declaration* decl : *declarations){
+        //generate ASM
+      }
+
+      for(Statement* expr : *statements){
+          //generate ASM
+      }
+
+      o << "leave" << endl;
+      o << "ret" << endl;
 }
