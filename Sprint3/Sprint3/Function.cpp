eeb02c333075d7ebd3eff198a7ba1741e40eefb0
MarionAlbinet <44368792+MarionAlbinet@users.noreply.github.com>
2019-04-02 08:29:48 +0200

    ADD S3 changes to S4-IR branch (#11)
    * [SPRINT5][ADD]Grammar for s5 with types (char int) and function def and call
    
    * [SPRINT5][RM]type stays but only with int (rm of char)
    
    * S3 back (#10)
    
    * [SPRINT3][ADD] Sprint 2 Back
    
    * [SPRINT3][ADD] Affectation asm
    
    * [SPRINT3][ADD] Addition
    
    * [SPRINT3][ADD] Substraction
    
    * [SPRINT3][ADD] Base for multiplication and division
    
    * [SPRINT3][ADD] Addition en chaîne
    
    * [SPRINT3][FIX] Bug pour addition à trois termes + refactoring
    
    * [SPRINT3][REWORK] ASM and sprint 3 is finished sa mère la maman
    
    * [SPRINT3][FIX] Fix a bug that wouldn't exist if everybody had a amazing apple computer

--- Sprint3/Sprint3/Function.cpp
+++ Sprint3/Sprint3/Function.cpp
@@ -1,1 +1,31 @@
 #include "Function.h"
+#include <iostream>
+
+Function::Function(vector<Declaration *> *declarations, vector<Statement *> *stats, StatementReturn *rstat){
+ this->declarations = declarations;
+ this->statements = stats;  
+ this->rstat = rstat;
+
+}
+
+void Function::buildASM(ostream &o){
+
+    int N;
+    N = declarations->size()*4 +16;
+      o << ".text" << endl;
+      o << ".global main" << endl;
+      o << "main:" << endl;
+      o << "pushq %rbp" << endl;
+      o << "movq    %rsp, %rbp" << endl;
+
+      for(Declaration* decl : *declarations){
+        decl->buildASM(o,&SymbolIndex, &lastOffset);
+      }
+      for(Statement* stat : *statements){
+        stat->buildASM(o,&SymbolIndex, &lastOffset);
+      }
+      rstat->buildASM(o,&SymbolIndex, &lastOffset);
+
+      o << "popq    %rbp" << endl;
+      o << "ret" << endl;
+}
