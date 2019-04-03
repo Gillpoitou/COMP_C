ad5da30dd9e5bcf827eba66840b413c962f5f1d6
Gillpoitou <etibossk@gmail.com>
2019-04-03 11:54:51 +0200

    [S5-VISITORS][ADD]add function's parameters to symbol table

--- Sprint4/Sprint4/Function.cpp
+++ Sprint4/Sprint4/Function.cpp
@@ -3,9 +3,13 @@
 void Function::build_IR(CFG* ir_cfg){
 
     BasicBlock* first_bb = new BasicBlock(ir_cfg,this->name);
-//TODO add param to symbol table
+
     ir_cfg->add_bb(first_bb);
     ir_cfg->current_bb = first_bb;
+
+    for(ExpressionVar* exp : *params){
+        ir_cfg->add_to_symbol_table(exp->name, INT);
+    }
 	
     for(Declaration* decl : *declarations){
        decl->build_IR(ir_cfg);
